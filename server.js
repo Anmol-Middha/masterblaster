aa80daff66dcc6214457df7993f5c78dd52c4f31
Anmol Middha <anmolmiddha2827@gmail.com>
2019-11-05 17:50:04 +0530

    new commit

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ const fetchDataMiddleware = require('./api/middleware/fetchData');
 
 app.use('/sachin', fetchDataMiddleware ,sachinAnalysisRoute);
 
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use((req, res, next)=>{
     const error = new Error('Not Found');
     error.status = 404;
